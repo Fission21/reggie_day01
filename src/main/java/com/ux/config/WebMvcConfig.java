7557efe72b9e9d5e233064885a9a9a0645ce15fd
Fission21 <blackhwcf@gmail.com>
2023-02-17 14:23:08 +0800

    优化了几个导包

--- src/main/java/com/ux/config/WebMvcConfig.java
+++ src/main/java/com/ux/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package com.ux.config;
 
+import com.ux.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -16,5 +22,19 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("放行resource路径资源");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 扩展Mvc消息转化器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，底层使用jackson将Java对象转为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将上面的消息转换器对象追加到mvc框架的转换器集合中,设置为 0 优先使用JacksonObjectMapper 我们自己写的
+        converters.add(0,messageConverter);
     }
 }
